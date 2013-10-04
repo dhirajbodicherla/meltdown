9528ce875469c2bbe9e2b888ff915bdb409965f3
Dhiraj Bodicherla <dhiraj.bodicherla@gmail.com>
2013-10-03 23:00:31 -0400

    1. Can dynamically create objects on the tiles

---
+++ Assets/scripts/Tile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnMouseDown () {
+		GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+		cylinder.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        cylinder.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y+0.6f, gameObject.transform.position.z);
+
+	}
+}
